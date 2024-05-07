73fa839ef2dec61e9eb561b3d26e7d74d270e27a
blacklight44 <yakupyakut@yandex.com>
2024-05-07 16:16:34 +0300

    adding links and home notfound components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; //module içi route
+import { NotFoundComponent } from './not-found/not-found.component'; //module içi route
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }, //hiç bir pathle eşleşmiyorsa
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { Chapter1Module } from './chapter1/chapter1.module'; //module import
 import { Chapter2Module } from './chapter2/chapter2.module'; //module import
 import { Chapter3Module } from './chapter3/chapter3.module'; //module import
 import { Chapter4Module } from './chapter4/chapter4.module'; //module import
-import { Chapter5Module } from './chapter5/chapter5.module'; //module import
+import { Chapter5Module } from './chapter5/chapter5.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component'; //module import
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     Chapter1Module,
     Chapter2Module,
     Chapter3Module,
     Chapter4Module,
     Chapter5Module,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
